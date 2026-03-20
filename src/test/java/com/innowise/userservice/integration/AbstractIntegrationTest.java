4a8f2327ab487fa934a403874fc9d80c3d17a5fb
Pavel Kiryanov <PavelKirjanov.007@mail.ru>
2026-03-21 01:17:46 +0300

    Fix CI #5

--- src/test/java/com/innowise/userservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/innowise/userservice/integration/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.innowise.userservice.integration;
 
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -14,32 +13,25 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "classpath:clean-db.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
-
-    @Container
-    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
-            .withExposedPorts(6379);
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.url", () -> "jdbc:tc:postgresql:16-alpine:///testdb");
+        registry.add("spring.datasource.username", () -> "test");
+        registry.add("spring.datasource.password", () -> "test");
         registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
 
-        registry.add("spring.data.redis.host", redis::getHost);
-        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379).toString());
-
+        registry.add("spring.data.redis.host", () -> "localhost");
+        registry.add("spring.data.redis.port", () -> {
+            try (var redis = new org.testcontainers.containers.GenericContainer<>("redis:7-alpine")
+                    .withExposedPorts(6379)) {
+                redis.start();
+                return redis.getMappedPort(6379);
+            }
+        });
 
         registry.add("spring.liquibase.enabled", () -> "true");
-
         registry.add("spring.cache.type", () -> "redis");
         registry.add("spring.cache.redis.time-to-live", () -> "60000");
     }
